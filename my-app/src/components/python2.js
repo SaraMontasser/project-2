26056fa1292dfb151470d13150e87f87f02c4c11
sara-montasser <saramontasser4@gmail.com>
2022-09-11 20:10:28 +0200

    feat: Render second python course page

--- my-app/src/components/python2.js
+++ my-app/src/components/python2.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import Footer from './footer'
 import LoadingSpinner from './loadingSpinner';
-export default function python2() {
+import CourseInfo from './CourseInfo'
+import Learn from './Learn';
+import CourseContent from './CourseContent';
+import Requirements from './Requirements';
+import Description from './Description';
+import Instructors from './Instructors';
+import Reviews from './Reviews';
+import StudentFeedback from './StudentFeedback';
+export default function python2(props) {
+  if(props.data==null){
+    return (
+      <LoadingSpinner />
+    )
+  }
+  else{
+    const courseData=props.data[1];
   return (
     <div>
-        vdfrevnjui
+        <CourseInfo courseinfo={courseData}/>
+        <Learn courseinfo={courseData.learn}/>
+        <CourseContent courseinfo={courseData.lectures}/>
+        <Requirements courseinfo={courseData.requirements}/>
+        <Description courseinfo={courseData.descriptions}/>
+        <Instructors courseinfo={courseData.instructors}/>
+        <StudentFeedback />
+        <Reviews courseinfo={courseData.reviews} />
+        
         <Footer />
     </div>
   )
+  }
 }
